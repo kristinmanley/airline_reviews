bebedd5d4b7fd1446fcc797b7ad587b9042fa9b6
Raghu Betina <raghu@firstdraft.com>
2017-03-12 19:45:26 +0000

    Direct association between routes and flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,8 @@
 class Flight < ApplicationRecord
   # Direct associations
 
+  belongs_to :route
+
   # Indirect associations
 
   # Validations
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,6 +1,9 @@
 class Route < ApplicationRecord
   # Direct associations
 
+  has_many   :flights,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
