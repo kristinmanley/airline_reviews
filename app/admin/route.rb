fb8594dc66714af91592ceabf730137bdcce6497
Raghu Betina <raghu@firstdraft.com>
2017-03-12 19:45:40 +0000

    Whitelist columns in Route for ActiveAdmin

--- app/admin/route.rb
+++ app/admin/route.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Route do
 
+ permit_params :departure_airport, :destination_airport, :route_name
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
